16a5cc73793b8f2af99fdde56b401bcac4264881
DanilaTing <tinggone@gmail.com>
2017-05-17 10:44:47 +0300

    ispravil

---
+++ javascripts/history.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.museumHistory').click(function () {
+    $('.museumArchitectureArticle').addClass('hidden')
+    $('.museumHistoryArticle').removeClass('hidden')
+  });
+
+  $('.museumArchitecture').click(function () {
+    $('.museumArchitectureArticle').removeClass('hidden')
+    $('.museumHistoryArticle').addClass('hidden')
+  });
+});
