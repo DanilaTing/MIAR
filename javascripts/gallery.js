772c0d9ddc1a91cb0210ed868c0ad0d2d3f0005e
DanilaTing <tinggone@gmail.com>
2017-05-21 04:02:56 +0300

    gallery created

--- javascripts/gallery.js
+++ javascripts/gallery.js
@@ -1,52 +1,62 @@
 $(function functionName() {
 
-  var images = $('.gallery img').length;
+  var images = $('.gallery img').length - 1;
   console.log(images);
-  //
-  var m = -1;
-  function check() {
-    var real = -1 <= m <= images;
-    console.log(real);
-  }
-
-  // function lol() {
-  //   function countGallery() {
-  //     if (real) {
-  //       m = m + 1;
-  //       console.log(m);
-  //     } else {
-  //       m = 0;
-  //       console.log(m);
-  //     }
-  //     // setTimeout(countGallery(), 5000);
-  //   };
-  //
-  //   countGallery()
-  //
-  //   $(function image () {
-  //     $('.gallery img').eq(m).addClass("visible");
-  //     // setTimeout(image(), 5000);
-  //   });
-  //
-  // }
 
   var m = -1;
 
-  $('.gallery img').eq(1).addClass("visible");
+  $('.layout').mouseenter(function () {
+    $('.buttons').css({'display':'inline-flex'});
+  })
+  $('.layout').mouseleave(function () {
+    $('.buttons').css({'display':'none'});
+  })
 
-  $('.gallery img').click(function () {
-    check();
-    if (-1 <= m < 4) {
+  $('.gallery img').eq(3).addClass("visible");
+  $('.gallery img').not($('.gallery img').eq(3)).removeClass("visible");
+
+  $(function func() {
+    if ( m < images) {
       m++ ;
       $('.gallery img').eq(m).addClass("visible");
       $('.gallery img').not($('.gallery img').eq(m)).removeClass("visible");
-      console.log(m);
-    } else if (m = 3) {
-      check();
-      $('.gallery img').eq(1).addClass("visible");
-      $('.gallery img').not($('.gallery img').eq(m)).removeClass("visible");
+      console.log('m' + m);
+    } else {
       m = 0;
-      console.log(m);
+      $('.gallery img').eq(m).addClass("visible");
+      $('.gallery img').not($('.gallery img').eq(m)).removeClass("visible");
+      console.log('m' + m);
+    };
+
+    var timerId = setTimeout (func, 5000);
+    timerId
+  });
+
+  $('.buttons div:first-of-type').click( function () {
+    if ( 0 < m ) {
+      m-- ;
+      $('.gallery img').eq(m).addClass("visible");
+      $('.gallery img').not($('.gallery img').eq(m)).removeClass("visible");
+      console.log('m' + m);
+    } else {
+      m = images;
+      $('.gallery img').eq(m).addClass("visible");
+      $('.gallery img').not($('.gallery img').eq(m)).removeClass("visible");
+      console.log('m' + m);
+    };
+  });
+
+  $('.buttons div:last-of-type').click( function () {
+    if ( m < images ) {
+      m++ ;
+      $('.gallery img').eq(m).addClass("visible");
+      $('.gallery img').not($('.gallery img').eq(m)).removeClass("visible");
+      console.log('m' + m);
+    } else {
+      m = 0;
+      $('.gallery img').eq(m).addClass("visible");
+      $('.gallery img').not($('.gallery img').eq(m)).removeClass("visible");
+      console.log('m' + m);
     };
   });
 
